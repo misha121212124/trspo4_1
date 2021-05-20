e418908a0f4bf7d61ea325101232ac82ccdb33ed
misha121212124 <38782002+misha121212124@users.noreply.github.com>
2021-05-20 22:38:12 +0300

    Update Brain.java

--- Brain.java
+++ Brain.java
@@ -5,10 +5,12 @@ public interface Brain extends Remote {
     String enableShowIntermediateResultsMode() throws RemoteException;
     String disableShowIntermediateResultsMode() throws RemoteException;
     void getL10(int n) throws RemoteException;
+    void getL12(int n, double min, double max) throws RemoteException;
     void getL16(int n) throws RemoteException;
     void getL17(int n, double min, double max) throws RemoteException;
     void getL30(double[][] a) throws RemoteException;
     void getL32() throws RemoteException;
     double[][] getL40() throws RemoteException;
     double[][] getL42() throws RemoteException;
+    double[][] getL50() throws RemoteException;
 }
